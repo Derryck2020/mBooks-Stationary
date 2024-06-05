7bc6b3919fcbeb7740a91b58b9d3f6eaedf3b550
Derrick <oseiagyemanb@gmail.com>
2024-06-05 19:52:31 +0000

    addition of image files

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import logo_img from './mbooks-logo.svg';
+import about_hero from './undraw_hero.svg';
+import step_1 from './undraw_1.svg';
+import step_2 from './undraw_order_delivered.svg';
+
+export { logo_img, about_hero, step_1, step_2 };
