a424aefa6622b92161664e4a5761dca1dcec5967
Derrick <oseiagyemanb@gmail.com>
2024-06-05 19:54:25 +0000

    update of file

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,16 @@
+import { Link } from 'react-router-dom';
+import { LibraryBig } from 'lucide-react';
+import { logo_img } from '@/assets/images';
+
 const Logo = () => {
-	return <div>Logo</div>;
+	return (
+		<Link
+			to='/'
+			className='hidden lg:flex justify-center items-center p-2 gap-2'
+		>
+			<LibraryBig className='w-9 h-9 bg-[#0099FF] rounded-md p-1 text-white' />
+			<img src={logo_img} alt='logo-image' className='w-20' />
+		</Link>
+	);
 };
 export default Logo;
