50f2a5c179124401ae20c2b4730b0346100caf1d
Derrick <oseiagyemanb@gmail.com>
2024-06-07 14:52:25 +0000

    update of file

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ const Logo = () => {
 			to='/'
 			className='hidden lg:flex justify-center items-center p-2 gap-2'
 		>
-			<LibraryBig className='w-9 h-9 bg-[#0099FF] rounded-md p-1 text-white' />
+			<LibraryBig className='w-9 h-9 bg-[#0099FF] rounded-md p-1 -ml-2 text-white' />
 			<img src={logo_img} alt='logo-image' className='w-20' />
 		</Link>
 	);
