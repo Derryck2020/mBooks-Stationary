f9260adb9e188781e292d7b9958ccdf9bc952efe
Derrick <oseiagyemanb@gmail.com>
2024-06-07 14:49:39 +0000

    initial commit of assets file

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,5 +2,20 @@ import logo_img from './mbooks-logo.svg';
 import about_hero from './undraw_hero.svg';
 import step_1 from './undraw_1.svg';
 import step_2 from './undraw_order_delivered.svg';
+import hero_img_1 from './hero-images/aki-ola-cmaths.png';
+import hero_img_2 from './hero-images/sharpener.png';
+import hero_img_3 from './hero-images/golden-eng-5.png';
+import hero_img_4 from './hero-images/sharpener.png';
+import hero_img_5 from './hero-images/sci-side.jpg';
 
-export { logo_img, about_hero, step_1, step_2 };
+export {
+	logo_img,
+	about_hero,
+	step_1,
+	step_2,
+	hero_img_1,
+	hero_img_2,
+	hero_img_3,
+	hero_img_4,
+	hero_img_5,
+};
