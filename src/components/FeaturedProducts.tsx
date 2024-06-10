60b256300fda47fcd5e3fc632221d7f5d0c43275
Derrick <oseiagyemanb@gmail.com>
2024-06-10 22:12:19 +0000

    update of file

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,15 +1,32 @@
 import { SectionTitle, ProductsGrid } from '.';
+import { type ProductsResponse } from '@/utils';
 
-const FeaturedProducts = () => {
+const FeaturedProducts = ({
+	products,
+}: {
+	products: { [key: string]: ProductsResponse };
+}) => {
 	return (
 		<>
 			<section className='pt-24'>
-				<SectionTitle text='Recommended Senior High School Books' />
-				<ProductsGrid />
+				<SectionTitle text='Senior High School Books' />
+				<ProductsGrid products={products.seniorHigh.data} />
 			</section>
-			<section className='pt-24'>
-				<SectionTitle text='Selected Junior High School Learning Products' />
-				<ProductsGrid />
+			<section className='pt-20'>
+				<SectionTitle text='Junior High School Books' />
+				<ProductsGrid products={products.juniorHigh.data} />
+			</section>
+			<section className='pt-20'>
+				<SectionTitle text='Primary School Books' />
+				<ProductsGrid products={products.upperLowerPrimary.data} />
+			</section>
+			<section className='pt-20'>
+				<SectionTitle text='Kindergarten and Nursery Books' />
+				<ProductsGrid products={products.kindergartenNursery.data} />
+			</section>
+			<section className='pt-20'>
+				<SectionTitle text='Stationery' />
+				<ProductsGrid products={products.stationery.data} />
 			</section>
 		</>
 	);
