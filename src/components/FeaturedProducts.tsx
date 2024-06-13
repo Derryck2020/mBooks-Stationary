4f464010b0d8d6f0a1a4adc4ca5ac288bb9c6312
Derrick <oseiagyemanb@gmail.com>
2024-06-13 11:48:12 +0000

    update of file

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -5,28 +5,35 @@ const FeaturedProducts = ({
 	products,
 }: {
 	products: { [key: string]: ProductsResponse };
+	limit?: number;
 }) => {
 	return (
 		<>
 			<section className='pt-24'>
 				<SectionTitle text='Senior High School Books' />
-				<ProductsGrid products={products.seniorHigh.data} />
+				<ProductsGrid products={products.seniorHigh.data} limit={4} />
 			</section>
 			<section className='pt-20'>
 				<SectionTitle text='Junior High School Books' />
-				<ProductsGrid products={products.juniorHigh.data} />
+				<ProductsGrid products={products.juniorHigh.data} limit={4} />
 			</section>
 			<section className='pt-20'>
 				<SectionTitle text='Primary School Books' />
-				<ProductsGrid products={products.upperLowerPrimary.data} />
+				<ProductsGrid
+					products={products.upperLowerPrimary.data}
+					limit={4}
+				/>
 			</section>
 			<section className='pt-20'>
 				<SectionTitle text='Kindergarten and Nursery Books' />
-				<ProductsGrid products={products.kindergartenNursery.data} />
+				<ProductsGrid
+					products={products.kindergartenNursery.data}
+					limit={4}
+				/>
 			</section>
 			<section className='pt-20'>
 				<SectionTitle text='Stationery' />
-				<ProductsGrid products={products.stationery.data} />
+				<ProductsGrid products={products.stationery.data} limit={4} />
 			</section>
 		</>
 	);
